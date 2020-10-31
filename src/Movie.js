be596f304b370fde754a54e0cece5dd2ea19dab3
이용현 <dydwk231@naver.com>
2020-10-31 16:52:53 +0900

    MoviePost component

--- src/Movie.js
+++ src/Movie.js
@@ -1,13 +1,21 @@
 import React from "react";
+import MoviePost from "./MoviePost";
+import PropTypes from "prop-types";
 
 const Movie = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <img src={props.poster} />
+      <MoviePost img={props.poster} />
       <h1>{props.date}</h1>
     </div>
   );
+};
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  date: PropTypes.number.isRequired,
 };
 
 export default Movie;
